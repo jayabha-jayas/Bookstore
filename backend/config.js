aa69d3e7ec76019319b9a7eb139dc4dbfeb66c19
Jayabharathi S <jayabharathi.s@thoughtworks.com>
2024-07-24 17:58:41 +0530

    Add mongodb initial setup

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const port = 5555;
+
+export const mongoDBURL = 'mongodb+srv://jayabhajayas:0M3kXtnlikTvExYS@cluster0.rjdilco.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 import express from "express";
-import { port } from "./config.js";
+import { port, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(port, () => {
-    console.log(`App is listening to port : ${port}`);
+app.get('/', (request, response) => {
+    console.log(`request is ${request}`);
+    return response.status(234).send('Welcome to bookstore');
 });
+
+mongoose
+    .connect(mongoDBURL)
+    .then(
+        () => {
+            console.log("App connected to the database");
+            app.listen(port, () => {
+                console.log(`App is listening to port : ${port}`);
+            });
+        }
+    ).catch(
+        (error) => {
+            console.log(error);
+        }
+    );
