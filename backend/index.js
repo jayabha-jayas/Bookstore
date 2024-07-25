2278867d496743098da75232c19ebe4296003e7f
Jayabharathi S <jayabharathi.s@thoughtworks.com>
2024-07-25 10:35:02 +0530

    GET PUT POST DELETE api calls

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,71 @@ app.get('/', (request, response) => {
     return response.status(234).send('Welcome to bookstore');
 });
 
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+        return response.status(234).send({ count: books.length, data: books });
+    }
+
+    catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
+});
+
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const book = await Book.findById(id);
+        return response.status(234).send(book);
+    }
+
+    catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
+});
+
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const { id } = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+        if (!result) {
+            return response.status(404).send({ message: "Book not found" });
+        }
+        return response.status(200).send({ message: "Book updated succesfully" });
+    }
+
+    catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
+});
+
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const result = await Book.findByIdAndDelete(id);
+        if (!result) {
+            return response.status(404).send({ message: "Book not found" });
+        }
+        return response.status(200).send({ message: "Book deleted succesfully" });
+    }
+
+    catch (error) {
+        console.log(error.message);
+        return response.status(500).send({ message: error.message });
+    }
+});
 
 app.post('/books', async (request, response) => {
     try {
